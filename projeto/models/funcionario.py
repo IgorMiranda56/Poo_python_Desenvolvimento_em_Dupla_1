bbc8cf79f33691a034b1d2cdacf27a7b86995e04
Felipe Santos <159580121+felipesantos71@users.noreply.github.com>
2024-10-07 12:29:03 +0000

    att 1.3

--- projeto/models/funcionario.py
+++ projeto/models/funcionario.py
@@ -6,8 +6,8 @@ class Funcionario(ABC):
     def __init__(self, nome: str, telefone: str, email: str, salario: float, endereco: Endereco) -> None:
         super().__init__()
         self.nome = self._verificar_nome(nome)
-        self.telefone = telefone
-        self.email = email
+        self.telefone = self._verificar_telefone(telefone)
+        self.email = self._verificar_email(email)
         self.salario = self._verificar_salario(salario)
         self.endereco = endereco
 
@@ -21,8 +21,18 @@ class Funcionario(ABC):
         if not isinstance(nome, str):
             raise TypeError("Digite apenas texto para os nome.")
         if not nome.strip():
-            raise ValueError("Nome precisa conter informação.")
+            raise ValueError("Nome não pode conter espaços vazios.")
         return nome
+    
+    def _verificar_telefone(self, telefone: str) -> str:
+        if not telefone.strip():
+            raise ValueError("Telefone não pode conter espaços vazios.")
+        return telefone
+    
+    def _verificar_email(self, email: str) -> str:
+        if not email.strip():
+            raise ValueError("Email não pode conter espaços vazios.")
+        return email
     
     def _verificar_salario(self, salario:float) -> float:
         if not isinstance(salario, float):
@@ -30,8 +40,6 @@ class Funcionario(ABC):
         if salario < 0:
             raise ValueError("Digite apenas numeros positivos para salario.")
         return salario 
-
-
 
 #criando tostring()
     def __str__(self) -> str:
